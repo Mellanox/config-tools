deb2651c87fde7b98c9e3e03f5512aaec46c8e1c
Andrey Yevsyukov <andreyy@mellanox.com>
2015-12-03 14:14:28 +0200

    Python API moved to separate Ubuntu package

--- deb-build.sh
+++ deb-build.sh
@@ -4,8 +4,7 @@
                        mlnx_src=/opt/mellanox/src
   mlnx_sf_mellanox_config_tools=mellanox-config-tools
         mlnx_sd_mlnx_udev_namer=mlnx-udev-namer
-         mlnx_sd_mellanoxconfig=mellanoxconfig
-   mlnx_sf_mellanoxconfig_setup=mellanoxconfig-setup.py
+                 mlnx_sd_python=python
 
 mlnx_bin=deb${mlnx_bin}
 mlnx_src=deb${mlnx_src}
@@ -15,8 +14,7 @@ mkdir -p ${mlnx_bin} ${mlnx_src}
 cp -f ${mlnx_sf_mellanox_config_tools} ${mlnx_bin}
 chmod +x ${mlnx_bin}/${mlnx_sf_mellanox_config_tools}
 cp -rf ${mlnx_sd_mlnx_udev_namer} ${mlnx_src}
-cp -rf ${mlnx_sd_mellanoxconfig} ${mlnx_src}
-cp -f ${mlnx_sf_mellanoxconfig_setup} ${mlnx_src}
+cp -rf ${mlnx_sd_python} ${mlnx_src}
 cd deb
 dpkg-buildpackage -b -tc -uc
 cd -
